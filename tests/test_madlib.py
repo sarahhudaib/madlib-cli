1a7ef22606e46b4a6928eadccba8ac072befa0fe
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-03-07 15:17:44 +0300

    100% solved

--- tests/test_madlib.py
+++ tests/test_madlib.py
@@ -2,21 +2,16 @@ import pytest
 from madlib_cli.madlib import read_template, parse, merge
 
 def test_read_template_returns_stripped_string():
-    actual = read_template()
-    received = read_template()
+    actual = read_template("madlib_cli/assets/template.txt")
+    received = read_template("madlib_cli/assets/template.txt")
     assert actual == received
 
 
 @pytest.mark.skip("pending")
 def test_parse_template():
-    actual_stripped, actual_parts = parse(
-        "It was a {Adjective} and {Adjective} {Noun}."
-    )
-    expected_stripped = "It was a {} and {} {}."
-    expected_parts = ("Adjective", "Adjective", "Noun")
-
-    assert actual_stripped == expected_stripped
-    assert actual_parts == expected_parts
+    expected = ["first name","age"]
+    received = parse("hello {first name}, I am {age} years old")
+    assert expected == received
 
 
 @pytest.mark.skip("pending")
